29ff75e7ff6f8389f11b6a9b5d471eecb97771b2
Ruslan <info@mirasov.dev>
2026-08-10 13:32:15 +0200

    init

--- goit-js-hw-07/js/task-3.js
+++
@@ -1,9 +0,0 @@
-const nameInput = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#name-output');
-
-function onNameInput(e) {
-  const name = e.target.value.trim();
-  nameOutput.textContent = name || 'Anonymous';
-}
-
-nameInput.addEventListener('input', onNameInput);
